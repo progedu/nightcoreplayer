8c10318bb5ff021605b5408adc5760129eef26c2
apprec100 <apprec100@outlook.com>
2018-04-11 16:20:57 +0900

    H,J,K,L キーで back,play,pause,forward ができるようにした

--- src/main/scala-2.11/jp/ed/nnn/nightcoreplayer/ToolbarCreator.scala
+++ src/main/scala-2.11/jp/ed/nnn/nightcoreplayer/ToolbarCreator.scala
@@ -5,15 +5,44 @@ import javafx.event.{ActionEvent, EventHandler}
 import javafx.geometry.Pos
 import javafx.scene.Scene
 import javafx.scene.control.{Label, TableView}
+import javafx.scene.input.{KeyCode, KeyEvent}
 import javafx.scene.layout.HBox
 import javafx.scene.media.MediaView
 import javafx.stage.Stage
 import javafx.util.Duration
-
 import jp.ed.nnn.nightcoreplayer.SizeConstants._
 import jp.ed.nnn.nightcoreplayer.ToolbarButtonCreator.createButton
 
 object ToolbarCreator {
+
+  private[this] def back(mediaView: MediaView): Unit = {
+    if (mediaView.getMediaPlayer != null) {
+      mediaView.getMediaPlayer.seek(
+        mediaView.getMediaPlayer.getCurrentTime.subtract(new Duration(10000)))
+    }
+  }
+
+  private[this] def play(mediaView: MediaView, tableView: TableView[Movie]): Unit ={
+    val selectionModel = tableView.getSelectionModel
+    if (mediaView.getMediaPlayer != null && !selectionModel.isEmpty) {
+      mediaView.getMediaPlayer.play()
+    }
+  }
+
+  private[this] def pause(mediaView: MediaView){
+    if (mediaView.getMediaPlayer != null) {
+      mediaView.getMediaPlayer.pause()
+    }
+  }
+
+  private[this] def forward(mediaView: MediaView) = {
+    if (mediaView.getMediaPlayer != null) {
+      val player = mediaView.getMediaPlayer
+      if (player.getTotalDuration.greaterThan(player.getCurrentTime.add(new Duration(10000)))) {
+        player.seek(player.getCurrentTime.add(new Duration(10000)))
+      }
+    }
+  }
 
   def create(mediaView: MediaView, tableView: TableView[Movie], timeLabel: Label, scene: Scene, primaryStage: Stage): HBox = {
     val toolBar = new HBox()
@@ -31,39 +60,22 @@ object ToolbarCreator {
 
     // back button
     val backButton = createButton("back.png", new EventHandler[ActionEvent]() {
-      override def handle(event: ActionEvent): Unit =
-        if (mediaView.getMediaPlayer != null) {
-          mediaView.getMediaPlayer.seek(
-            mediaView.getMediaPlayer.getCurrentTime.subtract(new Duration(10000)))
-        }
+      override def handle(event: ActionEvent): Unit = back(mediaView)
     })
 
     // play button
     val playButton = createButton("play.png", new EventHandler[ActionEvent]() {
-      override def handle(event: ActionEvent): Unit = {
-        val selectionModel = tableView.getSelectionModel
-        if (mediaView.getMediaPlayer != null && !selectionModel.isEmpty) {
-          mediaView.getMediaPlayer.play()
-        }
-      }
+      override def handle(event: ActionEvent): Unit = play(mediaView, tableView)
     })
 
     // pause button
     val pauseButton = createButton("pause.png", new EventHandler[ActionEvent]() {
-      override def handle(event: ActionEvent): Unit = {
-        if (mediaView.getMediaPlayer != null) mediaView.getMediaPlayer.pause()
-      }
+      override def handle(event: ActionEvent): Unit = pause(mediaView)
     })
 
     // forward button
     val forwardButton = createButton("forward.png", new EventHandler[ActionEvent]() {
-      override def handle(event: ActionEvent): Unit =
-        if (mediaView.getMediaPlayer != null) {
-            val player = mediaView.getMediaPlayer
-            if (player.getTotalDuration.greaterThan(player.getCurrentTime.add(new Duration(10000)))) {
-              player.seek(player.getCurrentTime.add(new Duration(10000)))
-            }
-        }
+      override def handle(event: ActionEvent): Unit = forward(mediaView)
     })
 
     // last button
@@ -93,6 +105,18 @@ object ToolbarCreator {
           mediaView.fitHeightProperty().bind(scene.heightProperty().subtract(toolBarMinHeight))
           mediaView.fitWidthProperty().bind(scene.widthProperty().subtract(tableMinWidth))
         }        
+    })
+
+    scene.setOnKeyPressed(new EventHandler [KeyEvent] () {
+      override def handle (event: KeyEvent) = {
+        event.getCode match {
+          case KeyCode.H => back(mediaView)
+          case KeyCode.J => play(mediaView, tableView)
+          case KeyCode.K => pause(mediaView)
+          case KeyCode.L => forward(mediaView)
+          case _         => // System.out.println(event.getCode)
+        }
+      }
     })
 
     toolBar.getChildren.addAll(
