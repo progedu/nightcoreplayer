f3e9b33dba61fe92479df013306fa1526ccd9e9d
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-04-23 20:35:15 +0900

    ToolbarCreatorリファクタリング

---
+++ src/main/scala/jp/ed/nnn/nightcoreplayer/ToolbarCreator.scala
@@ -0,0 +1,75 @@
+package jp.ed.nnn.nightcoreplayer
+
+import javafx.event.{ActionEvent, EventHandler}
+import javafx.geometry.Pos
+import javafx.scene.control.{Label, TableView}
+import javafx.scene.layout.HBox
+import javafx.scene.media.MediaView
+import javafx.stage.Stage
+import javafx.util.Duration
+import jp.ed.nnn.nightcoreplayer.SizeConstants._
+import jp.ed.nnn.nightcoreplayer.ToolbarButtonCreator.createButton
+
+object ToolbarCreator {
+  def create(mediaView: MediaView, tableView: TableView[Movie], timeLabel: Label, primaryStage: Stage): HBox = {
+    val toolBar = new HBox()
+    toolBar.setMinHeight(toolBarMinHeight)
+    toolBar.setAlignment(Pos.CENTER)
+    toolBar.setStyle("-fx-background-color: Black")
+
+    // first button
+    val firstButton = createButton("first.png", new EventHandler[ActionEvent]() {
+      override def handle(event: ActionEvent): Unit =
+        if (mediaView.getMediaPlayer != null) {
+          MoviePlayer.playPre(tableView, mediaView, timeLabel)
+        }
+    })
+    // back button
+    val backButton = createButton("back.png", new EventHandler[ActionEvent]() {
+      override def handle(event: ActionEvent): Unit =
+        if (mediaView.getMediaPlayer != null) {
+          mediaView.getMediaPlayer.seek(
+            mediaView.getMediaPlayer.getCurrentTime.subtract(new Duration(10000)))
+        }
+    })
+    // play button
+    val playButton = createButton("play.png", new EventHandler[ActionEvent]() {
+      override def handle(event: ActionEvent): Unit = {
+        val selectionModel = tableView.getSelectionModel
+        if (mediaView.getMediaPlayer != null && !selectionModel.isEmpty) {
+          mediaView.getMediaPlayer.play()
+        }
+      }
+    })
+    // pause button
+    val pauseButton = createButton("pause.png", new EventHandler[ActionEvent]() {
+      override def handle(event: ActionEvent): Unit = {
+        if (mediaView.getMediaPlayer != null) mediaView.getMediaPlayer.pause()
+      }
+    })
+    // forward button
+    val forwardButton = createButton("forward.png", new EventHandler[ActionEvent]() {
+      override def handle(event: ActionEvent): Unit =
+        if (mediaView.getMediaPlayer != null) {
+          mediaView.getMediaPlayer.seek(
+            mediaView.getMediaPlayer.getCurrentTime.add(new Duration(10000)))
+        }
+    })
+    // last button
+    val lastButton = createButton("last.png", new EventHandler[ActionEvent]() {
+      override def handle(event: ActionEvent): Unit =
+        if (mediaView.getMediaPlayer != null) {
+          MoviePlayer.playNext(tableView, mediaView, timeLabel)
+        }
+    })
+    // fullscreen button
+    val fullscreenButton = createButton("fullscreen.png", new EventHandler[ActionEvent]() {
+      override def handle(event: ActionEvent): Unit =
+        primaryStage.setFullScreen(true)
+    })
+
+    toolBar.getChildren.addAll(
+      firstButton, backButton, playButton, pauseButton, forwardButton, lastButton, fullscreenButton, timeLabel)
+    toolBar
+  }
+}
