ee44542f62f76181d92420e12e5f43b452c69723
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-05-27 23:42:01 +0900

    不具合修正と停止ボタン追加

--- src/main/scala/jp/ed/nnn/nightcoreplayer/ToolbarCreator.scala
+++ src/main/scala/jp/ed/nnn/nightcoreplayer/ToolbarCreator.scala
@@ -1,7 +1,9 @@
 package jp.ed.nnn.nightcoreplayer
 
+import javafx.beans.value.{ChangeListener, ObservableValue}
 import javafx.event.{ActionEvent, EventHandler}
 import javafx.geometry.Pos
+import javafx.scene.Scene
 import javafx.scene.control.{Label, TableView}
 import javafx.scene.layout.HBox
 import javafx.scene.media.MediaView
@@ -11,7 +13,7 @@ import jp.ed.nnn.nightcoreplayer.SizeConstants._
 import jp.ed.nnn.nightcoreplayer.ToolbarButtonCreator.createButton
 
 object ToolbarCreator {
-  def create(mediaView: MediaView, tableView: TableView[Movie], timeLabel: Label, primaryStage: Stage): HBox = {
+  def create(mediaView: MediaView, tableView: TableView[Movie], timeLabel: Label, scene: Scene, primaryStage: Stage): HBox = {
     val toolBar = new HBox()
     toolBar.setMinHeight(toolBarMinHeight)
     toolBar.setAlignment(Pos.CENTER)
@@ -47,6 +49,12 @@ object ToolbarCreator {
         if (mediaView.getMediaPlayer != null) mediaView.getMediaPlayer.pause()
       }
     })
+    // stop button
+    val stopButton = createButton("STOP.png", new EventHandler[ActionEvent]() {
+      override def handle(event: ActionEvent): Unit = {
+        if (mediaView.getMediaPlayer != null) mediaView.getMediaPlayer.stop()
+      }
+    })
     // forward button
     val forwardButton = createButton("forward.png", new EventHandler[ActionEvent]() {
       override def handle(event: ActionEvent): Unit =
@@ -66,12 +74,27 @@ object ToolbarCreator {
     })
     // fullscreen button
     val fullscreenButton = createButton("fullscreen.png", new EventHandler[ActionEvent]() {
-      override def handle(event: ActionEvent): Unit =
+      override def handle(event: ActionEvent): Unit = {
         primaryStage.setFullScreen(true)
+        mediaView.fitHeightProperty().unbind()
+        mediaView.fitHeightProperty().bind(scene.heightProperty())
+        mediaView.fitWidthProperty().unbind()
+        mediaView.fitWidthProperty().bind(scene.widthProperty())
+      }
+    })
+    primaryStage.fullScreenProperty().addListener(new ChangeListener[java.lang.Boolean] {
+      override def changed(observable: ObservableValue[_ <: java.lang.Boolean], oldValue: java.lang.Boolean, newValue: java.lang.Boolean): Unit =
+        if (!newValue) {
+          mediaView.fitHeightProperty().unbind()
+          mediaView.fitWidthProperty().unbind()
+          mediaView.fitHeightProperty().bind(scene.heightProperty().subtract(toolBarMinHeight))
+          mediaView.fitWidthProperty().bind(scene.widthProperty().subtract(tableMinWidth))
+        }
     })
 
     toolBar.getChildren.addAll(
-      firstButton, backButton, playButton, pauseButton, forwardButton, lastButton, fullscreenButton, timeLabel)
+      firstButton, backButton, playButton, pauseButton, stopButton, forwardButton, lastButton, fullscreenButton, timeLabel)
     toolBar
+
   }
 }
