7a29f757c7c0c16e016485426c1930f4cdec7547
morukamu <h7qefr@gmail.com>
2020-08-30 23:06:52 +0900

    オリジナルの実装

--- src/main/scala-2.11/jp/ed/nnn/nightcoreplayer/Main.scala
+++ src/main/scala-2.11/jp/ed/nnn/nightcoreplayer/Main.scala
@@ -1,20 +1,19 @@
 package jp.ed.nnn.nightcoreplayer
 
-import java.io.File
 import javafx.application.Application
-import javafx.beans.value.{ChangeListener, ObservableValue}
 import javafx.collections.FXCollections
 import javafx.event.EventHandler
-import javafx.geometry.Pos
 import javafx.scene.Scene
 import javafx.scene.control.cell.PropertyValueFactory
 import javafx.scene.control._
-import javafx.scene.input.{DragEvent, MouseEvent, TransferMode}
-import javafx.scene.layout.{BorderPane, HBox}
-import javafx.scene.media.{Media, MediaPlayer, MediaView}
+import javafx.scene.input.MouseEvent
+import javafx.scene.layout.BorderPane
+import javafx.scene.media.MediaView
 import javafx.scene.paint.Color
 import javafx.stage.Stage
-import javafx.util.{Callback, Duration}
+import javafx.util.Callback
+
+import jp.ed.nnn.nightcoreplayer.SizeConstants._
 
 object Main extends App {
   Application.launch(classOf[Main], args: _*)
@@ -22,22 +21,12 @@ object Main extends App {
 
 class Main extends Application {
 
-  private[this] val mediaViewFitWidth = 800
-  private[this] val mediaViewFitHeight = 450
-  private[this] val toolBarMinHeight = 50
-  private[this] val tableMinWidth = 300
-
   override def start(primaryStage: Stage): Unit = {
     val mediaView = new MediaView()
 
     val timeLabel = new Label()
     timeLabel.setText("00:00:00/00:00:00")
     timeLabel.setTextFill(Color.WHITE)
-
-    val toolBar = new HBox(timeLabel)
-    toolBar.setMinHeight(toolBarMinHeight)
-    toolBar.setAlignment(Pos.CENTER)
-    toolBar.setStyle("-fx-background-color: Black")
 
     val tableView = new TableView[Movie]()
     tableView.setMinWidth(tableMinWidth)
@@ -49,14 +38,13 @@ class Main extends Application {
         row.setOnMouseClicked(new EventHandler[MouseEvent] {
           override def handle(event: MouseEvent): Unit = {
             if (event.getClickCount >= 1 && !row.isEmpty) {
-              playMovie(row.getItem, tableView, mediaView, timeLabel)
+              MoviePlayer.play(row.getItem, tableView, mediaView, timeLabel)
             }
           }
         })
         row
       }
     })
-
 
     val fileNameColumn = new TableColumn[Movie, String]("ファイル名")
     fileNameColumn.setCellValueFactory(new PropertyValueFactory("fileName"))
@@ -76,101 +64,23 @@ class Main extends Application {
     tableView.getColumns.setAll(fileNameColumn, timeColumn, deleteActionColumn)
 
     val baseBorderPane = new BorderPane()
-    baseBorderPane.setStyle("-fx-background-color: Black")
+    val scene = new Scene(baseBorderPane, mediaViewFitWidth + tableMinWidth, mediaViewFitHeight + toolBarMinHeight)
+    val toolBar = ToolbarCreator.create(mediaView, tableView, timeLabel, scene, primaryStage)
+    baseBorderPane.setStyle("-fx-background-color: Red")
     baseBorderPane.setCenter(mediaView)
     baseBorderPane.setBottom(toolBar)
     baseBorderPane.setRight(tableView)
 
-    val scene = new Scene(baseBorderPane, mediaViewFitWidth + tableMinWidth, mediaViewFitHeight + toolBarMinHeight)
     scene.setFill(Color.BLACK)
     mediaView.fitWidthProperty().bind(scene.widthProperty().subtract(tableMinWidth))
     mediaView.fitHeightProperty().bind(scene.heightProperty().subtract(toolBarMinHeight))
 
-    scene.setOnDragOver(new EventHandler[DragEvent] {
-      override def handle(event: DragEvent): Unit = {
-        if (event.getGestureSource != scene &&
-          event.getDragboard.hasFiles) {
-          event.acceptTransferModes(TransferMode.COPY_OR_MOVE: _*)
-        }
-        event.consume()
-      }
-    })
-
-    scene.setOnDragDropped(new EventHandler[DragEvent] {
-      override def handle(event: DragEvent): Unit = {
-        val db = event.getDragboard
-        if (db.hasFiles) {
-          db.getFiles.toArray(Array[File]()).toSeq.foreach { f =>
-            val filePath = f.getAbsolutePath
-            val fileName = f.getName
-            val media = new Media(f.toURI.toString)
-            val player = new MediaPlayer(media)
-            player.setOnReady(new Runnable {
-              override def run(): Unit = {
-                val time = formatTime(media.getDuration)
-                val movie = Movie(System.currentTimeMillis(), fileName, time, filePath, media)
-                while (movies.contains(movie)) {
-                  movie.setId(movie.getId + 1L)
-                }
-                movies.add(movie)
-                player.dispose()
-              }
-            })
-          }
-        }
-        event.consume()
-      }
-    })
+    scene.setOnDragOver(new MovieFileDragOverEventHandler(scene))
+    scene.setOnDragDropped(new MovieFileDragDroppedEventHandler(movies))
 
     primaryStage.setTitle("mp4ファイルをドラッグ&ドロップしてください")
 
     primaryStage.setScene(scene)
     primaryStage.show()
   }
-
-  private[this] def playMovie(movie: Movie, tableView: TableView[Movie], mediaView: MediaView, timeLabel: Label): Unit = {
-    if (mediaView.getMediaPlayer != null) {
-      val oldPlayer = mediaView.getMediaPlayer
-      oldPlayer.stop()
-      oldPlayer.dispose()
-    }
-
-    val mediaPlayer = new MediaPlayer(movie.media)
-    mediaPlayer.currentTimeProperty().addListener(new ChangeListener[Duration] {
-      override def changed(observable: ObservableValue[_ <: Duration], oldValue: Duration, newValue: Duration): Unit =
-        timeLabel.setText(formatTime(mediaPlayer.getCurrentTime, mediaPlayer.getTotalDuration))
-    })
-    mediaPlayer.setOnReady(new Runnable {
-      override def run(): Unit =
-        timeLabel.setText(formatTime(mediaPlayer.getCurrentTime, mediaPlayer.getTotalDuration))
-    })
-    mediaPlayer.setOnEndOfMedia(new Runnable {
-      override def run(): Unit = playNext(tableView, mediaView, timeLabel)
-    })
-
-    mediaView.setMediaPlayer(mediaPlayer)
-    mediaPlayer.setRate(1.25)
-    mediaPlayer.play()
-  }
-
-  private[this] def playNext(tableView: TableView[Movie], mediaView: MediaView, timeLabel: Label): Unit = {
-    val selectionModel = tableView.getSelectionModel
-    if (selectionModel.isEmpty) return
-    val index = selectionModel.getSelectedIndex
-    val nextIndex = (index + 1) % tableView.getItems.size()
-    selectionModel.select(nextIndex)
-    val movie = selectionModel.getSelectedItem
-    playMovie(movie, tableView, mediaView, timeLabel)
-  }
-
-  private[this] def formatTime(elapsed: Duration): String = {
-    "%02d:%02d:%02d".format(
-      elapsed.toHours.toInt,
-      elapsed.toMinutes.toInt % 60,
-      elapsed.toSeconds.toInt % 60
-    )
-  }
-
-  private[this] def formatTime(elapsed: Duration, duration: Duration): String =
-    s"${formatTime(elapsed)}/${formatTime(duration)}"
 }
---
+++ src/main/scala-2.11/jp/ed/nnn/nightcoreplayer/SizeConstants.scala
@@ -0,0 +1,10 @@
+package jp.ed.nnn.nightcoreplayer
+
+object SizeConstants {
+
+  val mediaViewFitWidth = 800
+  val mediaViewFitHeight = 850
+  val toolBarMinHeight = 50
+  val tableMinWidth = 300
+
+}
