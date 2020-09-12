dc3806cef2af0568995767a3a0277ce66fd53873
ko-kamenashi <kamenashi0917@gmail.com>
2020-09-12 15:20:34 +0900

    chore: add movie file
    to learn observer pattern

--- src/main/scala/jp/ed/nnn/nightcoreplayer/Main.scala
+++ src/main/scala/jp/ed/nnn/nightcoreplayer/Main.scala
@@ -19,7 +19,7 @@ object Main extends App {
 class Main extends Application {
 
   override def start(primaryStage: Stage): Unit = {
-    val path = "/Users/soichiro_yoshimura/vagrant/ubuntu64/workspace/youtube_download/video.mp4"
+    val path = "downloads/video.mp4"
     val media = new Media(new File(path).toURI.toString)
     val mediaPlayer = new MediaPlayer(media)
     mediaPlayer.setRate(1.25)
