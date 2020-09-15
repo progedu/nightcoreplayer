60a925d57730f17d49e6b1f6a29c4b5467aa42e4
ko-kamenashi <kamenashi0917@gmail.com>
2020-09-15 18:33:35 +0900

    feat: add media table code

--- src/main/scala/jp/ed/nnn/nightcoreplayer/Main.scala
+++ src/main/scala/jp/ed/nnn/nightcoreplayer/Main.scala
@@ -1,11 +1,14 @@
 package jp.ed.nnn.nightcoreplayer
 
 import java.io.File
+
 import javafx.application.Application
 import javafx.beans.value.{ChangeListener, ObservableValue}
+import javafx.collections.FXCollections
 import javafx.geometry.Pos
 import javafx.scene.Scene
-import javafx.scene.control.Label
+import javafx.scene.control.cell.PropertyValueFactory
+import javafx.scene.control.{Label, TableColumn, TableView}
 import javafx.scene.layout.{BorderPane, HBox}
 import javafx.scene.media.{Media, MediaPlayer, MediaView}
 import javafx.scene.paint.Color
@@ -20,6 +23,7 @@ class Main extends Application {
   private[this] val MediaViewFitWidth = 800
   private[this] val MediaViewFitHeight = 450
   private[this] val ToolBarMinHeight = 50
+  private[this] val TableMinWidth = 300
 
   override def start(primaryStage: Stage): Unit = {
     val path = "downloads/video.mp4"
@@ -51,16 +55,35 @@ class Main extends Application {
     toolBar.setAlignment(Pos.CENTER)
     toolBar.setStyle("-fx-background-color: Black")
 
+    val tableView = new TableView[Movie]()
+    tableView.setMinWidth(TableMinWidth)
+    val movies = FXCollections.observableArrayList[Movie]()
+    tableView.setItems(movies)
+
+    val fileNameColumn = new TableColumn[Movie, String]("ファイル名")
+    fileNameColumn.setCellValueFactory(new PropertyValueFactory("fileName"))
+    fileNameColumn.setPrefWidth(160)
+    val timeColumn = new TableColumn[Movie, String]("時間")
+    timeColumn.setCellValueFactory(new PropertyValueFactory("time"))
+    timeColumn.setPrefWidth(80)
+
+    tableView.getColumns.setAll(fileNameColumn, timeColumn)
+
+      // TODO 後で消す
+    movies.addAll(Movie(1L, "video.mp4", "00:00:00", "./video.mp4", null))
+
+
     val baseBorderPane = new BorderPane()
 
     baseBorderPane.setStyle("-fx-background-color: Black")
     baseBorderPane.setCenter(mediaView)
     baseBorderPane.setBottom(toolBar)
+    baseBorderPane.setRight(tableView)
 
-    val scene = new Scene(baseBorderPane, MediaViewFitWidth, MediaViewFitHeight + ToolBarMinHeight)
+    val scene = new Scene(baseBorderPane, MediaViewFitWidth + TableMinWidth, MediaViewFitHeight + ToolBarMinHeight)
 
     scene.setFill(Color.BLACK)
-    mediaView.fitWidthProperty().bind(scene.widthProperty())
+    mediaView.fitWidthProperty().bind(scene.widthProperty().subtract(TableMinWidth))
     mediaView.fitHeightProperty().bind(scene.heightProperty().subtract(ToolBarMinHeight))
 
     primaryStage.setScene(scene)
